b1a0afe07d6f8ae29e1b37c0c5b02f43857c6b24
yyy-anime <haygontt@gmail.com>
2025-08-20 00:14:31 +0700

    Create episodes.js

---
+++ episodes.js
@@ -0,0 +1,6 @@
+// episodes.js
+const episodes = {
+  94: "https://cdn.anime-bk.com/videos/ep94.mp4",
+  95: "https://cdn.anime-bk.com/videos/ep95.mp4",
+  96: "https://cdn.anime-bk.com/videos/ep96.mp4"
+};
